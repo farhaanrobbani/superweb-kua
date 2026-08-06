4316399d2c34052e02466943b1952b97786437fb
farhaanrobbani <farhaanrobbani@users.noreply.github.com>
2026-08-06 22:16:10 +0700

    revert: kembalikan tinggi iframe layanan ke awal

--- tests/Feature/ServicesAnnouncementsTest.php
+++ tests/Feature/ServicesAnnouncementsTest.php
@@ -306,8 +306,7 @@ class ServicesAnnouncementsTest extends TestCase
             ->assertOk()
             ->assertSee('Pencarian Akta')
             ->assertSee('https://datastudio.google.com/embed/reporting/a67ad441-873f-4189-8cca-d4e6325397ca/page/gPzuF')
-            ->assertSee('sandbox')
-            ->assertSee('height:100vh');
+            ->assertSee('sandbox');
     }
 
     public function test_cari_akta_page_without_embed_redirects_home(): void
